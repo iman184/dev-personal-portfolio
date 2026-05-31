3b85123714d69d424fbb640ca327cb19ef1140b2
Imen Zighed <zighedimen921@gmail.com>
2026-05-31 09:26:36 +0200

    first commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,12 @@
+export const Hero = () =>{
+    return (
+    <section>
+        <div className="relative min-h-screen flex items-center overflow-hidden">
+            {/* Bg */}
+            <div>
+                <img />
+            </div>
+        </div>
+    </section>
+);
+}
